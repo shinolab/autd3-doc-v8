1f098d70edf6d14219f5b11b0f0f59e324cd5ae8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-18 13:08:12 +0900

    fix modulation transform

--- src/codes/Users_Manual/modulation/transform_0.rs
+++ src/codes/Users_Manual/modulation/transform_0.rs
@@ -2,5 +2,5 @@
 use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main() {
-let m = Sine::new(150. * Hz).with_transform(|_dev, _i, d| d / 2);
+let m = Sine::new(150. * Hz).with_transform(|_i, d| d / 2);
 # }
