4ca3fc401860f5c86e3a4786d2b306ce3ca16a57
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-23 01:20:10 +0900

    update mod sample

--- src/codes/Users_Manual/modulation/radiation_0.rs
+++ src/codes/Users_Manual/modulation/radiation_0.rs
@@ -2,5 +2,5 @@
 # use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main() {
-let m = Sine::new(150. * Hz).with_radiation_pressure();
+let m = Sine::new(150 * Hz).with_radiation_pressure();
 # }
