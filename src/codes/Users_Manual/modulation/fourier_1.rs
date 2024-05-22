ce841060c26babacdf585412bd41fda16c561e24
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-23 01:12:53 +0900

    update fouirer sample

--- src/codes/Users_Manual/modulation/fourier_1.rs
+++ src/codes/Users_Manual/modulation/fourier_1.rs
@@ -2,5 +2,5 @@
 # use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main()  {
-let m = Sine::new(100. * Hz) + Sine::new(150. * Hz) + Sine::new(200. * Hz);
+let m = Sine::new(100 * Hz) + Sine::new(150 * Hz) + Sine::new(200 * Hz);
 # }
