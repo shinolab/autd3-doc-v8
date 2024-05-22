8e4434a9e92bbf2f02b7f616b7ef559659b79a84
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-23 00:54:06 +0900

    rm indent

--- src/codes/Users_Manual/device_1.cs
+++ src/codes/Users_Manual/device_1.cs
@@ -3,8 +3,8 @@
 ~using AUTD3Sharp.Utils;
 ~using var autd = new ControllerBuilder().AddDevice(new AUTD3(Vector3d.Zero)).Open(Nop.Builder());
 ~{
-  var tr = autd.Geometry[0][0];
-  ~}
+var tr = autd.Geometry[0][0];
+~}
 foreach (var tr in autd.Geometry[0])
 {
   // do something
--- src/codes/Users_Manual/geometry_4.cs
+++ src/codes/Users_Manual/geometry_4.cs
@@ -3,8 +3,8 @@
 ~using AUTD3Sharp.Utils;
 ~using var autd = new ControllerBuilder().AddDevice(new AUTD3(Vector3d.Zero)).Open(Nop.Builder());
 ~{
-  var dev = autd.Geometry[0];
-  ~}
+var dev = autd.Geometry[0];
+~}
 foreach (var dev in autd.Geometry)
 {
   // do something
