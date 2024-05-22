1423d98b96d2fb61f640ae29e6c89f127ad66a85
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-23 01:13:33 +0900

    update square sample

--- src/codes/Users_Manual/modulation/square_1.rs
+++ src/codes/Users_Manual/modulation/square_1.rs
@@ -2,7 +2,7 @@
 # use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main()  {
-let m = autd3::modulation::Square::new(150. * Hz)
+let m = autd3::modulation::Square::new(150 * Hz)
             .with_low(u8::MIN)
             .with_high(u8::MAX);
 # }
--- src/codes/Users_Manual/modulation/square_2.rs
+++ src/codes/Users_Manual/modulation/square_2.rs
@@ -2,6 +2,6 @@
 # use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main()  {
-let m = autd3::modulation::Square::new(150. * Hz)
+let m = autd3::modulation::Square::new(150 * Hz)
         .with_duty(0.5);
 # }
