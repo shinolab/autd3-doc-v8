b2cd5d007d6adc49749c9da61ee7385c8345ee92
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-23 01:11:08 +0900

    update sine sample

--- src/codes/Users_Manual/modulation/sine_1.rs
+++ src/codes/Users_Manual/modulation/sine_1.rs
@@ -2,7 +2,7 @@
 # use autd3::prelude::*;
 # #[allow(unused_variables)]
 # fn main()  {
-let m = autd3::modulation::Sine::new(150. * Hz)
+let m = autd3::modulation::Sine::new(150 * Hz)
             .with_intensity(u8::MAX)
             .with_offset(u8::MAX / 2);
 # }
