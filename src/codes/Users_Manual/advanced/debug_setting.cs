c0c45c535bec864d18d8d14f3126e8bd3a46159e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-14 11:28:01 +0900

    fix cs sample

--- src/codes/Users_Manual/advanced/debug_setting.cs
+++ src/codes/Users_Manual/advanced/debug_setting.cs
@@ -1,7 +1,6 @@
 ~using AUTD3Sharp;
 ~using AUTD3Sharp.Link;
 ~using AUTD3Sharp.Utils;
-~using AUTD3Sharp.NativeMethods;
 ~using var autd = Controller.Builder([new AUTD3(Vector3.Zero)]).Open(Nop.Builder());
 autd.Send(new DebugSettings(
     (dev, gpio) => gpio == GPIOOut.O0 ? DebugType.BaseSignal : DebugType.None
